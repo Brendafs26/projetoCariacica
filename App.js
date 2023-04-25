8c1c5781c40d005f3c8bca6033679e72f6ed7bcf
Brenda Ferreira <72113756+Brendafs26@users.noreply.github.com>
2023-04-24 22:06:58 -0300

    Iniciei o projeto

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image, SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Feather from 'react-native-vector-icons/Feather';
 
+const Tab = createBottomTabNavigator();
+
+
+<Tab.Screen
+name={Home}
+component={Home}
+options={{
+  tabBarIcon: ({color,size}) => {
+    return <Feather name="home" color={color} size={size}  />
+  },
+}}
+/>
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+     <View style={styles.card}>
+      <Image
+      style={styles.foto}
+      source={require('./assets/cariacica.jpg')}
+      />
+      <Text> Terra Maravilhosa</Text>
+      <Text></Text>
+     </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
+  foto:{
+    width:380,
+     height:300,
+     
+  },
+  card:{
+    padding:20,
+    backgroundColor:'pink',
+  }
 });
